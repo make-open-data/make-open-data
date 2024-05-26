caa233ddb22441f714a2fe85634d233a21d11a31
Soufiane <soufiane.khoudmi@gmail.com>
2024-05-26 11:40:35 +0200

    manual casting in dqt

--- macros/aggreger_dvf.sql
+++ macros/aggreger_dvf.sql
@@ -1,8 +1,8 @@
 {% macro aggreger_dvf(filtrer_dvf) %}
     SELECT 
         id_mutation,
-        SUM(surface_reelle_bati) AS total_surface,
-        SUM(nombre_pieces_principales) AS total_pieces
+        SUM(CAST(surface_reelle_bati AS numeric)) AS total_surface,
+        SUM(CAST(nombre_pieces_principales AS numeric)) AS total_pieces
     FROM 
         filtrer_dvf
     GROUP BY 
