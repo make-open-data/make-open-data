71bb5d4c6e773ceacc7025a94331f4ab80bc7adc
Soufiane <soufiane.khoudmi@gmail.com>
2024-06-20 10:57:05 +0200

    slice the aggregation process

--- macros/population/pivoter_logement.sql
+++ macros/population/pivoter_logement.sql
@@ -1,15 +1,31 @@
-{% macro pivoter_logement() %}    
+{% macro pivoter_logement(colonne_a_aggreger, colonne_index) %}    
 
-    select
-        code_commune_insee,
+  {% set colonne_a_aggreger_values_query %}
+    SELECT DISTINCT 
+    champs__valeur
+    FROM {{ ref('eclater_logement_communes') }} WHERE champs = '{{ colonne_a_aggreger }}'
+  {% endset %}
+
+  {% set colonne_a_aggreger_values = run_query(colonne_a_aggreger_values_query) %}
+
+  {% set colonne_a_aggreger_values_list = [] %}
+  {% for row in colonne_a_aggreger_values %}
+      {% do colonne_a_aggreger_values_list.append(row[0]) %}
+  {% endfor %}
+
+
+
+      select 
+
+        code_commune_insee as code_commune_insee_{{ colonne_index }},
         {{ dbt_utils.pivot(
             'champs__valeur',
-            dbt_utils.get_column_values(ref('eclater_logement_communes'), 'champs__valeur'),
+            colonne_a_aggreger_values_list,
             agg='sum',
             then_value='population_par_commune_champs_valeur',
         ) }}
     from 
-        {{ ref('eclater_logement_communes') }}
+        {{ ref('eclater_logement_communes') }} WHERE champs = '{{ colonne_a_aggreger }}'
     group by
         code_commune_insee
 
--- models/aggregation/aggreger_logement_communes.sql
+++ models/aggregation/aggreger_logement_communes.sql
@@ -1,10 +1,43 @@
+  -- depends_on: {{ ref('eclater_logement_communes') }}
+  
 {{ config(materialized='table') }}
 
-with aggregated as (
-    {{ pivoter_logement() }}
-)
+{% set colonnes_logement_query %}
+  SELECT DISTINCT 
+  lib_col
+  FROM {{ ref('logement_2020_codes') }}
+{% endset %}
+{% set colonnes_logement_resultat = run_query(colonnes_logement_query) %}
+
+{% set colonnes_cles = ['code_commune_insee', 'poids_du_logement', 'region_residence'] %}
+{% if execute %}
+  {% set colonnes_a_aggreger_list = [] %}
+  {% for row in colonnes_logement_resultat %}
+    {% if row[0] not in colonnes_cles %}
+      {% do colonnes_a_aggreger_list.append(row[0]) %}
+    {% endif %}
+  {% endfor %}
+{% endif %}
+
+
+with communes as (
+    SELECT DISTINCT code_commune_insee from {{ ref('libelle_colonnes_logement') }}
+  ),
+  aggregated as (
+
+     SELECT * 
+     FROM communes
+
+      {% for colonne_a_aggreger in colonnes_a_aggreger_list %}
+      
+        LEFT JOIN ( {{ pivoter_logement(colonne_a_aggreger, loop.index) }}) pivoter_logement_{{ loop.index }} 
+        ON communes.code_commune_insee = pivoter_logement_{{ loop.index }}.code_commune_insee_{{ loop.index }} 
+      
+      {% endfor %}
+  )
+
 
 SELECT 
-    * 
+    *  
 FROM
     aggregated
