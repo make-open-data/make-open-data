63fb5f8d2e6b5d59bcafd26268c08bc9290d0262
Soufiane <soufiane.khoudmi@gmail.com>
2024-06-26 18:36:10 +0200

    fix some previous analysis

--- analyses/geo/multiple_cp_per_com.sql
+++ analyses/geo/multiple_cp_per_com.sql
@@ -1,6 +1,6 @@
 --- Montre les communes qui ont plusieurs codes postaux
 
 SELECT code_commune_insee, ARRAY_AGG(DISTINCT code_postal) as postal_codes
-FROM codes_postaux
+FROM {{ source('sources', 'cog_poste')}} 
 GROUP BY code_commune_insee
 HAVING COUNT(DISTINCT code_postal) > 1
