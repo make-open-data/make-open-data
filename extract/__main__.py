4a1cb4a3b8b839155fd862a75264f93529f19cac
Carole <carole@tenjin.com>
2024-06-29 12:28:28 +0200

    feat: ajoute la possibilite d'extracter seulement certaine sources
    - need the be tested

--- extract/__main__.py
+++ extract/__main__.py
@@ -11,10 +11,23 @@ from extract.loaders import read_from_source, upload_dataframe_to_table
 
 SOURCES_PATH = "sources.yml"
 
+def filter_sources_by_tag(sources, tag):
+    return {label: info for label, info in sources['sources'].items() if tag in info.get('tags', [])}
+
+
 if __name__ == "__main__":
 
+  # Load sources from YAML file
     with open(Path(os.path.dirname(__file__)) / SOURCES_PATH, "r") as ymlfile:
         sources = yaml.safe_load(ymlfile)
+
+    parser = argparse.ArgumentParser(description="Run extraction of data from sources.")
+    parser.add_argument('--tag', help="Extract only sources tagged with the specified tag")
+    args = parser.parse_args()
+
+    if args.tag:
+        # Filter sources by the specified tag
+        sources = filter_sources_by_tag(sources, args.tag)
 
     for source_label, source_infos in sources.items():
         with NamedTemporaryFile(suffix='.csv', delete=True) as tmpfile:
