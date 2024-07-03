c770f4289755040a090b093e07129077f3fcf462
Soufiane <soufiane.khoudmi@gmail.com>
2024-07-04 00:03:53 +0200

    add infos from communes model

--- models/prep/sante/aggeger_effectifs_sante_departement_2022.sql
+++ models/prep/sante/aggeger_effectifs_sante_departement_2022.sql
@@ -9,7 +9,7 @@ with aggreger_effectif_sante_unpivot as (
 		profession_sante,
 		sum(cast(effectif as numeric)) as effectif
 	FROM {{ source('sources', 'professionels_sante') }}
-	where (annee = '2022') and (libelle_departement != 'Tout département')
+	where (annee = '2022') and (libelle_departement != 'Tout département')  and (libelle_departement != 'FRANCE')
 	group by region, departement, libelle_region, libelle_departement, profession_sante
 ),
 aggreger_effectif_sante_departements as (
@@ -26,7 +26,23 @@ aggreger_effectif_sante_departements as (
 		) }}
   	FROM aggreger_effectif_sante_unpivot
 	group by region, libelle_region, departement, libelle_departement
+),
+infos_par_departement as (
+	select 
+		code_departement,
+		sum(cast(population as numeric)) as population_departement,
+		avg(commune_latitude) as latitude_centre_departement,
+		avg(commune_longitude) as longitude_centre_departement
+	from {{ ref('geo_communes') }}
+	group by code_departement
 )
 
-select * from aggreger_effectif_sante_departements
+select 
+	aggreger_effectif_sante_departements.*,
+	infos_par_departement.population_departement,
+	infos_par_departement.latitude_centre_departement,
+	infos_par_departement.longitude_centre_departement
+from aggreger_effectif_sante_departements
+left join infos_par_departement 
+on aggreger_effectif_sante_departements.departement = infos_par_departement.code_departement
 
