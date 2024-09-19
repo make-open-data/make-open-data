182c6c5180be0372b7ae865224f5f04e9baf11ec
Soufiane <soufiane.khoudmi@gmail.com>
2024-09-19 09:11:42 +0200

    fix mention to upper cases cols

--- 5_macros/recensement/concatener_logement.sql
+++ 5_macros/recensement/concatener_logement.sql
@@ -1,6 +1,6 @@
 {% macro concatener_logement(unpivoted, champs_geo, champs_geo_arrivee) %}    
     SELECT
-        '{{ champs_geo }}' as  {{ champs_geo_arrivee  }},
+        "{{ champs_geo }}" as  {{ champs_geo_arrivee  }},
         CAST("IPONDL" as NUMERIC) as poids_du_logement,
         champs,
         valeur,
