390dd569a962d406d9f4cf4220f72a8ab1255991
Soufiane <soufiane.khoudmi@gmail.com>
2024-10-09 10:39:53 +0200

    liste des tables en debut de doc

--- 1_data/simulations/fake_knn_data.sql
+++ 1_data/simulations/fake_knn_data.sql
@@ -3,8 +3,12 @@
 -- crée une fausse table avec id, latitude, longitude, et valeur columns
 -- S'assurer que le calcul ne se fait pas sur l'objet de la table
 
-{{ config(materialized='table') }}
-
+{{ 
+    config(
+        materialized='view',
+        schema='simulations'
+    ) 
+}}
 WITH fake_knn_table AS (
     SELECT 1 AS id, ST_SetSRID(ST_MakePoint(43.7, 3.832), 4326) AS geopoint, 100 AS valeur UNION ALL -- (3, 2) --> (200 + 300) / 2 = 250
     SELECT 3,       ST_SetSRID(ST_MakePoint(43.7, 3.830), 4326),             200 UNION ALL -- (1, 2) --> (100 + 300) / 2 = 200
