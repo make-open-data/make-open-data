9f92c3c9f9f323da0a5d1adcff618942aab05b78
Soufiane <soufiane.khoudmi@gmail.com>
2024-10-23 14:33:35 +0200

    eneleve knn5

--- 1_data/prepare/foncier/ventes_immobilieres_enrichies.sql
+++ 1_data/prepare/foncier/ventes_immobilieres_enrichies.sql
@@ -7,12 +7,4 @@
     ) 
 }}
 
-WITH dvf_knn_5 as(
-    {{ calculate_geo_knn('ventes_immobilieres', 'id_mutation', 'geopoint', 'prix_m2' , 5) }}
-)
-
-select ventes_immobilieres.*, 
-       dvf_knn_5.prix_m2_knn_5 
-from {{ ref('ventes_immobilieres') }} ventes_immobilieres
-left join dvf_knn_5 on dvf_knn_5.id = ventes_immobilieres.id_mutation
-
+select * from {{ ref('ventes_immobilieres') }}
