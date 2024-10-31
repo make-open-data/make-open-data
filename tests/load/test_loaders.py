292eb60583ae48fe4d0172a7bc7c30d789e0f50a
Carmen Alvarez <carmen@rmen.ca>
2024-10-31 16:50:18 +0100

    test : paramétrise le test `test_load_file_to_pg`
    Rend la fonction de test paramétrable, permettant ainsi de vérifier plusieurs types de fichiers différents avec la même fonction.

--- tests/load/test_loaders.py
+++ tests/load/test_loaders.py
@@ -1,3 +1,4 @@
+import dataclasses
 import importlib
 from typing import Any
 
@@ -20,8 +21,39 @@ def setup_module():
     importlib.reload(loaders)
 
 
+@dataclasses.dataclass
+class Scenario:
+    csv_filename: str
+    delimiter: str
+    expected_row_count: int
+    expected_first_row_data: dict[str, Any]
+
+
+SCENARIOS = [
+    Scenario(
+        csv_filename="base-officielle-codes-postaux",
+        delimiter=",",
+        expected_row_count=9,
+        expected_first_row_data={
+            "code_commune_insee": "69381",
+            "nom_de_la_commune": "LYON 01",
+            "code_postal": "69001",
+            "libelle_d_acheminement": "LYON",
+            "ligne_5": None,
+            "_geopoint": "45.770104,4.826373",
+        }
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    argnames=["scenario"],
+    ids=[scenario.csv_filename for scenario in SCENARIOS],
+    argvalues=[[scenario] for scenario in SCENARIOS],
+)
 def test_load_file_to_pg(
     db_connection: psycopg.Connection,
+    scenario: Scenario,
 ):
     """
     Étant donné un fichier csv,
@@ -32,26 +64,18 @@ def test_load_file_to_pg(
     # Étant donné un fichier csv,
     # Quand la fonction load_file_to_pg est appelée avec ce fichier,
     load_file_to_pg(
-        tmpfile_csv_path="tests/resources/base-officielle-codes-postaux.csv",
+        tmpfile_csv_path=f"tests/resources/{scenario.csv_filename}.csv",
         pg_table="test_load_file_to_pg",
         data_infos={
             "db_schema": "test_sources",
-            "csv_delimiter": ","
+            "csv_delimiter": scenario.delimiter,
             "file_format": "csv",
         },
     )
     with (db_connection.cursor(row_factory=rows.dict_row) as cursor):
         result: Cursor = cursor.execute("SELECT * from test_sources.test_load_file_to_pg")
 
-        assert result.rowcount == 9
+        assert result.rowcount == scenario.expected_row_count
         actual_first_row_data: list[dict[str, Any]] = result.fetchone()
-        expected_first_row_data = {
-            "code_commune_insee": "69381",
-            "nom_de_la_commune": "LYON 01",
-            "code_postal": "69001",
-            "libelle_d_acheminement": "LYON",
-            "ligne_5": None,
-            "_geopoint": "45.770104,4.826373",
-        }
 
-        assert actual_first_row_data == expected_first_row_data
+        assert actual_first_row_data == scenario.expected_first_row_data
